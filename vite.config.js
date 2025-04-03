6cfffa3508bdd9bcc070adc64f94afd684f4e6f9
prabhathkaluarachchi <fmprabhath@gmail.com>
2025-04-03 12:53:43 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/LibriSyncFrontend/'
+  base: '/LibriSyncFrontend/' // Must match your repo name exactly
 })
